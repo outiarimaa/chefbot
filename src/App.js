3918c16b3ccbbd63fd272a0b12d0960710438f44
taappana <herranen.jani@gmail.com>
2018-04-04 15:38:56 +0300

    Added navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import Header from './components/Header';
 import './App.css';
+import Navibar from './components/Navibar';
 
 class App extends Component {
 
   render() {
     return (
       <div>
+        <Navibar />
         <Header />
       </div>
     );
