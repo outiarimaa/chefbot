fbe7bf9bc29020b25f6177e4c3a6f147defa9b98
Jokatlin <lindh.j.k@gmail.com>
2018-04-05 10:42:36 +0300

    Added a login component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,101 +1,104 @@
-import React, { Component } from 'react';
-import { auth, googleProvider, facebookProvider } from '../firebase.js';
+import React, {Component} from 'react';
+import {auth, googleProvider, facebookProvider} from '../firebase.js';
 import '../App.css';
 import RecipeList from './RecipeList';
-import { Row, Col } from 'react-materialize';
+import {Row, Col} from 'react-materialize';
 import Navibar from './Navibar';
 import Login from './Login';
-import { Button } from 'react-materialize';
+import {Button} from 'react-materialize';
 
 class Header extends Component {
     constructor() {
-      super();
-      this.state = {
-        username: '',
-        user: null
-      }
-    }
-  
-    logout = () => {
-      auth.signOut()
-        .then(() => {
-          this.setState({
+        super();
+        this.state = {
+            username: '',
             user: null
-          });
-        });
-    }
-  
-    loginGoogle = () => {
-      auth.signInWithPopup(googleProvider)
-        .then((result) => {
-          const user = result.user;
-          this.setState({
-            user
-          });
-        });
-    }
-  
-    loginFacebook = () => {
-      auth.signInWithPopup(facebookProvider)
-        .then((result) => {
-          const user = result.user;
-          this.setState({
-            user
-          });
-        });
-    }
-  
-    componentDidMount() {
-      auth.onAuthStateChanged((user) => {
-        if (user) {
-          this.setState({ user });
         }
-      });
     }
-  
+
+    logout = () => {
+        auth.signOut()
+            .then(() => {
+                this.setState({
+                    user: null
+                });
+            });
+    }
+
+/*    loginGoogle = () => {
+        auth.signInWithPopup(googleProvider)
+            .then((result) => {
+                const user = result.user;
+                this.setState({
+                    user
+                });
+            });
+    }
+
+    loginFacebook = () => {
+        auth.signInWithPopup(facebookProvider)
+            .then((result) => {
+                const user = result.user;
+                this.setState({
+                    user
+                });
+            });
+    }*/
+
+    componentDidMount() {
+        auth.onAuthStateChanged((user) => {
+            if (user) {
+                this.setState({user});
+            }
+        });
+    }
+
     render() {
-      return (
-        <div>
+        return (
             <div>
-            <Navibar logout={this.logout} />
-              {this.state.user ?
                 <div>
+                    <Navibar logout={this.logout}/>
+                    {this.state.user ?
+                        <div>
 
-                  <Row id="aleksi">
-                    <Col s={6} className='left-column'>
+                            <Row id="aleksi">
+                                <Col s={6} className='left-column'>
 
-                  <p>Heippatirallaa {this.state.user.displayName}</p>
-                  <div>
-                  <div className="Chefbot-div">
-                    <iframe
-                        title="chefbot"
-                        width="100%"
-                        height="100%"
-                        src="https://console.dialogflow.com/api-client/demo/embedded/69d0c8a7-3ee8-444a-afb5-a514fb6a3bf1">
-                    </iframe>
-                  </div>
-                  </div>
-                    </Col>
-                    <Col s={6} className='right-column'>
-                        <RecipeList />
-                    </Col>
-                  </Row>
+                                    <p>Heippatirallaa {this.state.user.displayName}</p>
+                                    <div>
+                                        <div className="Chefbot-div">
+                                            <iframe
+                                                title="chefbot"
+                                                width="100%"
+                                                height="100%"
+                                                src="https://console.dialogflow.com/api-client/demo/embedded/69d0c8a7-3ee8-444a-afb5-a514fb6a3bf1">
+                                            </iframe>
+                                        </div>
+                                    </div>
+                                </Col>
+                                <Col s={6} className='right-column'>
+                                    <RecipeList/>
+                                </Col>
+                            </Row>
+                        </div>
+                        :
+                        <div>
+
+                            <Login/>
+                           {/* <div>
+
+                                <Button waves='light' className='red' onClick={this.loginGoogle}>Login with
+                                    Google</Button>
+                                <Button waves='light' className='blue' onClick={this.loginFacebook}>Login with
+                                    Facebook</Button>
+                                <p>Loggaa sisään, niin näätki jotain</p>
+                            </div>*/}
+                        </div>
+                    }
                 </div>
-                :
-                  <div>
-
-                  <Login omanimi={this.loginGoogle()}/>
-                  <div>
-                  <Button waves='light' className='red' onClick={this.loginGoogle}>Login with Google</Button>
-                  <Button waves='light' className='blue' onClick={this.loginFacebook}>Login with Facebook</Button>
-                  <p>Loggaa sisään, niin näätki jotain</p>
-                  </div>
-                      </div>
-               } 
             </div>
-        </div>
-      );
+        );
     }
-  }
-  
-  export default Header;
+}
+
+export default Header;
--- src/components/Login.js
+++ src/components/Login.js
@@ -2,8 +2,28 @@ import React, { Component } from 'react';
 import { Button } from 'react-materialize';
 import { auth } from '../firebase.js';
 import Navibar from "./Navibar";
+import {facebookProvider, googleProvider} from "../firebase";
 
 class Login extends Component {
+    loginGoogle = () => {
+        auth.signInWithPopup(googleProvider)
+            .then((result) => {
+                const user = result.user;
+                this.setState({
+                    user
+                });
+            });
+    }
+
+    loginFacebook = () => {
+        auth.signInWithPopup(facebookProvider)
+            .then((result) => {
+                const user = result.user;
+                this.setState({
+                    user
+                });
+            });
+    }
     render() {
         return (
 
@@ -11,7 +31,7 @@ class Login extends Component {
 
 
                     <div>
-                        <Button waves='light' className='red' onClick={this.props.omanimi}>Login with Google</Button>
+                        <Button waves='light' className='red' onClick={this.loginGoogle}>Login with Google</Button>
                         <Button waves='light' className='blue' onClick={this.loginFacebook}>Login with Facebook</Button>
                         <p>Loggaa sisään, niin näätki jotain</p>
                     </div>
