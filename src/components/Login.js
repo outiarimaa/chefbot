426ca36f99a6cc59ef0ff3eab141a36b14f2d8d5
Jokatlin <lindh.j.k@gmail.com>
2018-04-05 14:55:44 +0300

    Adding bootstrap for login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -28,32 +28,33 @@ class Login extends Component {
 
     render() {
         return (
-            <div className="loginpage">
-
-                <table>
-
-                    <tbody>
-                    <tr className="logintable" >
-
-                        <td className='introduction'>
-                            <h1>Welcome to ChefBot</h1>
-                            <img className="picture" src="http://fileserver.imagebucket.net/i/00000/5nnvtdsihay4_t.jpg"/>
-                            <p>ChefBot is an intelligent chatbot who can help you to find delicious recipes from a vast recipe database. ChefBot is still a child and keeps on learning in order to serve you better.</p>
-                        </td>
-                        <td className='signin'>
-                                <div className="loginbuttons">
-                                    <div><Button waves='light' id='google-button' onClick={this.loginGoogle}>Login with
-                                        Google</Button></div><br/>
-                                    <div><Button waves='light' id='fb-button' onClick={this.loginFacebook}>Login with
-                                        Facebook</Button></div>
-                                    <p>Please login in to use ChefBot</p>
-                                </div>
 
 
-                        </td>
-                    </tr>
-                    </tbody>
-                </table>
+            <div className="row loginpage">
+
+
+                <div className='introduction col-md-6'>
+                    <h1>Welcome to ChefBot</h1>
+                    <img className="picture"
+                         src="http://fileserver.imagebucket.net/i/00000/5nnvtdsihay4_t.jpg"/>
+                    <p>ChefBot is an intelligent chatbot, who can help you to find delicious recipes from a vast
+                        recipe database. ChefBot is still a child and keeps on learning in order to serve you
+                        better.</p>
+                </div>
+                <div className='signin col-md-6'>
+                    <div className="loginbuttons">
+                        <div><Button waves='light' id='google-button' onClick={this.loginGoogle}>Login with
+                            Google</Button></div>
+                        <br/>
+                        <div><Button waves='light' id='fb-button' onClick={this.loginFacebook}>Login with
+                            Facebook</Button></div>
+                        <p>Please log in to use ChefBot</p>
+                    </div>
+
+
+                </div>
+
+
             </div>
         );
     }
