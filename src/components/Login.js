db2bccf8a7a239c86d6d93bf31ff2006c76cdca9
Jokatlin <lindh.j.k@gmail.com>
2018-04-05 13:20:27 +0300

    Login page modification

--- src/components/Login.js
+++ src/components/Login.js
@@ -34,7 +34,7 @@ class Login extends Component {
 
                     <tbody>
                     <tr className="logintable" >
-                        <td className='introduction'>kjsdfklsdjfkl</td>
+                        <td className='introduction'></td>
                         <td className='signin'>
 
 
