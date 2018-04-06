d70109ea42854cdad970bfbf12a907187c13d1e3
Jokatlin <lindh.j.k@gmail.com>
2018-04-06 10:38:25 +0300

    Added a browserRouter and profile component and link to it

---
+++ src/components/Profile.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+
+class Profile extends Component {
+
+    render() {
+        return (
+            <div>
+                <h1>Hölökynkölökyn</h1>
+            </div>
+        );
+    }
+}
+
+export default Profile;
