db21ba3d480304b028f0ee11d9b5492e5a485e88
taappana <herranen.jani@gmail.com>
2018-04-09 14:45:19 +0300

    Profile component is shown again

--- src/components/Header.js
+++ src/components/Header.js
@@ -79,7 +79,6 @@ render() {
                                     </div>
                                 </div>
                             </nav>
-
                             <div style={ shown }>
                                 <div class="sidenav">
                                     <div className="Chefbot-div">
@@ -91,42 +90,33 @@ render() {
                                         </iframe>
                                     </div>
                                 </div>
-
                             </div>
                         </div>
-
-                            <div style={ shown }>
-                                <div className="main">
-                                    <div>
-                                        <div class="container-fluid bg-1 text-center">
+                        <div style={ shown }>
+                            <div className="main">
+                                <div>
+                                    <div class="container-fluid bg-1 text-center">
                                         <h3 id="cheftitle">CHEF<br/>BOT</h3>
-                                        </div>
-
-
-
-                        <div class="container-fluid bg-3 text-center">
-                            <p>Our all-time favorites</p>
-                        </div>
-
-                        <div class="container-fluid bg-2">
-                            <RecipeList/>
-
-                        </div>
-                </div>
-                  
-
-                <div style={ hidden }>
-                    <Profile state={this.state} />
-                </div>        
-                </div>
+                                    </div>
+                                    <div class="container-fluid bg-3 text-center">
+                                        <p>Our all-time favorites</p>
+                                    </div>
+                            <div class="container-fluid bg-2">
+                                <RecipeList/>
                             </div>
+                        </div>
                     </div>
-                    :
+                </div>
+                    <div style={ hidden }>
+                    <Profile state={this.state} />
+                    </div>        
+                </div>
+                :
                     <div>
                         <Login/>
                     </div>
                 }
-        </div>
+            </div>
         </div>
     );
 }
