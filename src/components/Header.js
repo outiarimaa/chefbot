1bf1b3c56e28d9c4596b06d09d72b1764d8eb5e7
taappana <herranen.jani@gmail.com>
2018-04-09 14:16:21 +0300

    Ordered header code

--- src/components/Header.js
+++ src/components/Header.js
@@ -43,85 +43,6 @@ class Header extends Component {
         });
     }
 
-    /* render() {
-        let shown = {
-			display: this.state.shown ? "block" : "none"
-		};
-		
-		let hidden = {
-			display: this.state.shown ? "none" : "block"
-		}
-        return (
-            <div>
-                <div>
-                    {this.state.user ?
-                        <div>
-                            <div class="container-fluid bg-1 text-center">
-                                <h3 id="cheftitle" class="margin">CHEF<br/>BOT</h3>
-
-                            </div>
-
-                            <div class="container-fluid bg-2 text-center">
-                                <h3>TALK TO ME!</h3><br/>
-
-                                        <div className="Chefbot-div">
-                                        <iframe
-                                        title="chefbot"
-                                        width="70%"
-                                        height="100%"
-                                        src="https://console.dialogflow.com/api-client/demo/embedded/69d0c8a7-3ee8-444a-afb5-a514fb6a3bf1">
-                                        </iframe>
-                                        </div>
-                                    </div>
-
-
-                        </div>
-                        :
-                        <div>
-
-                            <Login/>
-
-                            <div>
-                                <ButtonToolbar>
-                                    <DropdownButton title="" id="dropdown">
-                                    <MenuItem eventKey="1">
-                                    <div>
-                                        <div style={ shown } onClick={this.toggle.bind(this)}>Profile</div>
-                                        <div style={ hidden } onClick={this.toggle.bind(this)}>Home</div>
-                                    </div>
-                                    </MenuItem>
-                                    <MenuItem divider />
-                                    <MenuItem eventKey="2" onClick={this.logout}>Logout</MenuItem>
-                                    </DropdownButton>
-                                </ButtonToolbar>
-                                <p className="signin">Tervetuloa {this.state.user.displayName}!</p>
-                            </div>
-
-                            <div style={ shown }>
-                                <Row id="aleksi">
-                                    <Col s={6} className='left-column'>
-                                        <ChefBot />
-                                    </Col>
-                                    <Col s={6} className='right-column'>
-                                        <RecipeList />
-                                    </Col>
-                                </Row>
-                            </div>
-                            <div style={ hidden }>
-                                <Profile state={this.state} />
-                            </div>
-                        </div>
-                        :
-                        <div>
-                            <Login />
-                        </div>
-                    }
-                </div>
-            </div>
-        );
-    }
-} */
-
 render() {
     let shown = {
         display: this.state.shown ? "block" : "none"
@@ -134,62 +55,56 @@ render() {
         <div>
             <div>
                 {this.state.user ?
-                
-                <div>
-
                     <div>
-                <nav class="navbar navbar-default">
-                    <div class="container">
-                        <div class="navbar-header">
-                            <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-                                <span className="icon-bar"></span>
-                                <span className="icon-bar"></span>
-                                <span className="icon-bar"></span>
-                            </button>
-                            <a className="navbar-brand" onClick={this.logout}>Logout</a>
-                        </div>
-                        <div className="collapse navbar-collapse" id="myNavbar">
-                            <ul className="nav navbar-nav navbar-right">
-                                <li>
-                                    <div>
-                                        <div className="toggle-home-profile" style={ shown } onClick={this.toggle.bind(this)}>Profile</div>
-                                        <div className="toggle-home-profile" style={ hidden } onClick={this.toggle.bind(this)}>Home</div>
+                        <div>
+                            <nav class="navbar navbar-default">
+                                <div class="container">
+                                    <div class="navbar-header">
+                                        <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                                            <span className="icon-bar"></span>
+                                            <span className="icon-bar"></span>
+                                            <span className="icon-bar"></span>
+                                        </button>
+                                        <a className="navbar-brand" onClick={this.logout}>Logout</a>
                                     </div>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </nav>
-                <div style={ shown }>
-                        <div class="sidenav">
-                            <div className="Chefbot-div">
-                                <iframe
-                                    title="chefbot"
-                                    width="90%"
-                                    height="100%"
-                                    src="https://console.dialogflow.com/api-client/demo/embedded/69d0c8a7-3ee8-444a-afb5-a514fb6a3bf1">
-                                </iframe>
+                                    <div className="collapse navbar-collapse" id="myNavbar">
+                                        <ul className="nav navbar-nav navbar-right">
+                                            <li>
+                                                <div>
+                                                    <div className="toggle-home-profile" style={ shown } onClick={this.toggle.bind(this)}>Profile</div>
+                                                    <div className="toggle-home-profile" style={ hidden } onClick={this.toggle.bind(this)}>Home</div>
+                                                </div>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </nav>
+                            <div style={ shown }>
+                                <div class="sidenav">
+                                    <div className="Chefbot-div">
+                                        <iframe
+                                            title="chefbot"
+                                            width="90%"
+                                            height="100%"
+                                            src="https://console.dialogflow.com/api-client/demo/embedded/69d0c8a7-3ee8-444a-afb5-a514fb6a3bf1">
+                                        </iframe>
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </div>
-                <div style={ shown }>
-                    <div className="main">
+                            <div style={ shown }>
+                                <div className="main">
+                                    <div>
+                                        <div class="container-fluid bg-1 text-center">
+                                        <h3 id="cheftitle">CHEF<br/>BOT</h3>
 
-                    <div>
-                        <div class="container-fluid bg-1 text-center">
-                            <h3 id="cheftitle">CHEF<br/>BOT</h3>
-
-                        </div>
-
-                        <div class="container-fluid bg-2">
-                            {/*<h3>TALK TO ME!</h3><br/>*/}
+                                    </div>
+                                    <div class="container-fluid bg-2">
+                                        {/*<h3>TALK TO ME!</h3><br/>*/}
                             
+                                </div>
+                            </div>
                         </div>
-                </div>
-
-
-                    </div>
                     </div>
                 <div style={ hidden }>
                     <Profile state={this.state} />
@@ -197,9 +112,7 @@ render() {
                 </div>
                     :
                     <div>
-
                         <Login/>
-
                     </div>
                 }
             </div>
