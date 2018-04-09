d5eefa6dc7aeb5a0d8e3f12f7ee914d767df46c7
outiarimaa <outi.arimaa@gmail.com>
2018-04-09 10:52:53 +0300

    deleted router from app.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React, { Component } from 'react';
 import Header from './components/Header';
 import './components/App.css';
-import ShowPage from './components/ShowPage';
 
 class App extends Component {
 
   render() {
     return (
       <div>
-        {/* <Header /> */}
-        <ShowPage />
+         <Header />
       </div>
     );
   }
